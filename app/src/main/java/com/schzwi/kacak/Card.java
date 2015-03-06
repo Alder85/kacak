5e021ed8cebc24affb414c9a0468fba47dfd80b7
Alder85 <alder314@gmail.com>
2015-03-05 18:49:47 -0600

    Added dealing.

--- app/src/main/java/com/schzwi/kacak/Card.java
+++ app/src/main/java/com/schzwi/kacak/Card.java
@@ -1,11 +1,12 @@
 package com.schzwi.kacak;
 
 /**
+ * Creates a card object with a suit and a value
  * Created by Josh on 3/5/2015.
  */
 public class Card {
-    private int suit;
-    private int value;
+    private int suit = 0;
+    private int value = 0;
     private boolean activated;
 
     public Card(int suitIn, int valueIn) {
@@ -14,6 +15,15 @@ public class Card {
         if(valueIn > 0 && valueIn <= 13)
             value = valueIn;
 
+    }
+
+    public Card(Card x) {
+        suit = x.getSuit();
+        value = x.getValue();
+    }
+    public Card() { //null
+        suit = 0;
+        value = 0;
     }
 
     public int getAttack() {
@@ -25,6 +35,15 @@ public class Card {
             return 15;
         else //isTrap
             return 0;
+    }
+
+    public boolean isBlank() {
+        return suit == 0 && value == 0;
+    }
+
+    public void setBlank() {
+        suit = 0;
+        value = 0;
     }
 
     public int getDefense() {
@@ -41,27 +60,19 @@ public class Card {
     }
 
     public boolean isLowMonster() {
-        if(value >= 6 && value <= 9)
-            return true;
-        return false;
+        return value >= 6 && value <= 9;
     }
 
     public boolean isAce() {
-        if(value == 1)
-            return true;
-        return false;
+        return value == 1;
     }
 
     public boolean isFaceCard() {
-        if(value >= 11 && value <= 13)
-            return true;
-        return false;
+        return value >= 11 && value <= 13;
     }
 
     public boolean isTrap() {
-        if(value >= 2 && value <= 5)
-            return true;
-        return false;
+        return value >= 2 && value <= 5;
     }
 
     public int getSuit() {
@@ -70,6 +81,18 @@ public class Card {
 
     public int getValue() {
         return value;
+    }
+
+    public void setSuit(int x) {
+        suit = x;
+    }
+
+    public void setValue(int x) {
+        value = x;
+    }
+
+    public String toString() {
+        return "Suit = " + suit + "\nValue = " + value;
     }
 
 }
--- app/src/main/java/com/schzwi/kacak/singlePlayer.java
+++ app/src/main/java/com/schzwi/kacak/singlePlayer.java
@@ -11,19 +11,55 @@ public class singlePlayer extends ActionBarActivity {
     int heart = 1, club = 2, diamond = 3, spade = 4;
     int ace = 1, two = 2, three = 3, four = 4, five = 5, six = 6,
     seven = 7, eight = 8, nine = 9, ten = 10, jack = 11, queen = 12, king = 13;
-    Card[][] cardArray = new Card[4][12];
+    Card[] cardArray = new Card[52];
+    Card[] playerHand = new Card[26];
+    Card[] computerHand = new Card[26];  //half of default
+    Card[] discard = new Card[52]; //could hold entire deck if necessary
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single_player);
-
+        int count = 0;
         for(int i = 1;i < 5;i++) {  //1-4
-            for(int q = 1;q < 13;q++) {  //1- 12
-                cardArray[i][q] = new Card(i, q);
+            for(int q = 1;q < 14;q++) {  //1-13
+                cardArray[count] = new Card(i, q);
+                count++;
             }
         }
 
+        int currRand;
+        for (int i = 0; i < playerHand.length; i++) {
+            if (playerHand[i] == null) {
+                while (playerHand[i] == null) {
+                    currRand = randomInt();
+                    if(!(cardArray[currRand] == null)) {
+                        playerHand[i] = new Card(cardArray[currRand]);
+                        cardArray[currRand] = null;
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < computerHand.length; i++) {
+            if (computerHand[i] == null) {
+                while (computerHand[i] == null) {
+                    currRand = randomInt();
+                    if(!(cardArray[currRand] == null)) {
+                        computerHand[i] = new Card(cardArray[currRand]);
+                        cardArray[currRand] = null;
+                    }
+                }
+            }
+        }
+        Card potatoe = new Card();
     }
 
+    /**
+     * randomInt simplifies returning a specific random number
+     * @return A number between 0 and 51, inclusive (1 - 52)
+     */
+    public static int randomInt()
+    {
+        return (int)(Math.random() * 52);
+    }
 }
